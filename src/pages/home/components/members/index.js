4689316ded122303b84b6f48a98e0277e4a05e21
Ivan Gonzalez <ivangonzalezgrc@gmail.com>
2023-06-06 10:27:33 -0500

    Members component for home screen, adding, editing and deleting member

---
+++ src/pages/home/components/members/index.js
@@ -0,0 +1,53 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Button, HStack, Input, Pressable } from "native-base";
+import { MdDelete } from "react-icons/md";
+
+import Card from "../../../../components/card";
+
+const Members = (props) => {
+  const { members, onAddMember, onUpdateMember, onRemoveMember, onNext, nextDisabled, ...rest } = props;
+
+  return (
+    <Card {...rest}>
+      {members.map((member) => (
+        <HStack key={member.id} marginBottom={2}>
+          <Input
+            flex={1}
+            size="md"
+            value={member.name}
+            onChangeText={(name) => onUpdateMember(member.id, name)}
+            placeholder="Member name"
+          />
+          <Pressable
+            alignSelf="stretch"
+            justifyContent="center"
+            alignItems="center"
+            paddingX={1}
+            onPress={() => onRemoveMember(member.id)}
+          >
+            <MdDelete size={25} color="black" />
+          </Pressable>
+        </HStack>
+      ))}
+      <HStack alignItems="center" space={3}>
+        <Button flex={1} onPress={onAddMember} colorScheme="blue">
+          Add member
+        </Button>
+        <Button flex={1} onPress={onNext} colorScheme="green" isDisabled={nextDisabled}>
+          Next
+        </Button>
+      </HStack>
+    </Card>
+  );
+};
+
+Members.propTypes = {
+  members: PropTypes.array.isRequired,
+  onAddMember: PropTypes.func.isRequired,
+  onUpdateMember: PropTypes.func.isRequired,
+  onRemoveMember: PropTypes.func.isRequired,
+  onNext: PropTypes.func.isRequired,
+};
+
+export default React.memo(Members);
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import { Text, VStack } from "native-base";
 import Heading from "../../components/heading";
+import Members from "./components/members";
 
 const steps = {
   MEMBERS: 0,
@@ -10,6 +11,39 @@ const steps = {
 
 function HomePage() {
   const [step, setStep] = useState(steps.MEMBERS);
+  const [members, setMembers] = useState([]);
+
+  const onAddMember = useCallback(() => {
+    const _members = [...members];
+    _members.push({
+      id: String(new Date().getTime()),
+      name: "",
+    });
+    setMembers(_members);
+  }, [members]);
+
+  const onRemoveMember = useCallback(
+    (id) => {
+      let _members = [...members];
+      _members = _members.filter((_member) => _member.id !== id);
+      setMembers(_members);
+    },
+    [members]
+  );
+
+  const onUpdateMember = useCallback(
+    (id, name) => {
+      let _members = [...members];
+      _members = _members.map((_member) => {
+        if (_member.id === id) {
+          _member.name = name;
+        }
+        return _member;
+      });
+      setMembers(_members);
+    },
+    [members]
+  );
 
   return (
     <VStack safeArea paddingX={2} paddingY={2} overflowY="scroll" maxWidth={700} alignSelf="center" width="full">
@@ -17,18 +51,16 @@ function HomePage() {
         FairPay
       </Text>
       <Heading label="Members" isOpen={step === steps.MEMBERS} onPress={() => setStep(steps.MEMBERS)} />
-      <Heading
-        label="Orders"
-        isOpen={step === steps.ORDERS}
-        onPress={() => setStep(steps.ORDERS)}
-        // disabled={disabledOrders}
+      <Members
+        visible={step === steps.MEMBERS}
+        members={members}
+        onAddMember={onAddMember}
+        onUpdateMember={onUpdateMember}
+        onRemoveMember={onRemoveMember}
+        onNext={() => setStep(steps.ORDERS)}
       />
-      <Heading
-        label="Checkout"
-        isOpen={step === steps.CHECKOUT}
-        onPress={() => setStep(steps.CHECKOUT)}
-        // disabled={disabledCheckout}
-      />
+      <Heading label="Orders" isOpen={step === steps.ORDERS} onPress={() => setStep(steps.ORDERS)} />
+      <Heading label="Checkout" isOpen={step === steps.CHECKOUT} onPress={() => setStep(steps.CHECKOUT)} />
     </VStack>
   );
 }
