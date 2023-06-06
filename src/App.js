f5c6a18920c3a0283214749b07373b846c782b22
Ivan Gonzalez <ivangonzalezgrc@gmail.com>
2023-06-06 09:39:43 -0500

    Adding react router dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import routes from "./routes";
+import HomePage from "./pages/home";
+
 function App() {
-  return null;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={routes.home} element={<HomePage />} />
+        <Route path="*" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
